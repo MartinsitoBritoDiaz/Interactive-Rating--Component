33779d132910aab1e14905a3f87d803b07b1457c
Martinsito <martinsitobd@gmail.com>
2022-08-22 22:36:05 -0400

    Project finished

--- main.js
+++ main.js
@@ -1,11 +1,24 @@
 
 
-const sectionRate = document.querySelectorAll('.section--rate');
+let saveSelection; 
+const sectionRate = document.querySelectorAll('.section--rate .rate--button');
+const form = document.getElementById('form');
+const thankYouCard = document.getElementById('result');
 
 sectionRate.forEach((element, i) => {
     element.addEventListener('click', () => {
-        const btn = document.querySelectorAll(`#btn-${i+1}}`)
-        console.log(btn)
-
+        saveSelection = i+1;
     })
 });
+
+form.onsubmit = (e) => {
+    e.preventDefault();
+
+    if( saveSelection !== undefined ){
+        console.log(saveSelection)
+        const rating = document.getElementById('rating');
+        rating.innerHTML = saveSelection;
+        form.classList.toggle('active');        
+        thankYouCard.classList.toggle('active');        
+    }
+}
